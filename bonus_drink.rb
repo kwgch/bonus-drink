9e8fe921257fe131e3baf53c1e843c0a3048b074
kwgch <m.kwgch@gmail.com>
2014-08-04 14:38:26 +0900

    add answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    ret = 0
+    for num in 0..amount do
+      if num != 0
+        if num % 3 == 0 
+          ret += 1
+        end
+        ret += 1
+      end
+    end
+    ret
   end
 end
