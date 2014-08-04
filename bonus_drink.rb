36adb5892eff1d38dd78e123151a64c73ba321d3
kwgch <m.kwgch@gmail.com>
2014-08-04 15:05:30 +0900

    refactoring

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,18 +1,5 @@
 class BonusDrink
   def self.total_count_for(amount)
-<<<<<<< HEAD
-    amount + amount.pred.abs / 2
-=======
-    ret = 0
-    for num in 0..amount do
-      if num != 0
-        if num % 3 == 0 
-          ret += 1
-        end
-        ret += 1
-      end
-    end
-    ret
->>>>>>> master
+    amount + (amount/3).floor
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -6,21 +6,6 @@ describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(2)).to eq 2 }
   specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
   specify { expect(BonusDrink.total_count_for(4)).to eq 5 }
-<<<<<<< HEAD
-  specify { expect(BonusDrink.total_count_for(5)).to eq 7 }
-  specify { expect(BonusDrink.total_count_for(6)).to eq 8 }
-  specify { expect(BonusDrink.total_count_for(7)).to eq 10 }
-  specify { expect(BonusDrink.total_count_for(8)).to eq 11 }
-  specify { expect(BonusDrink.total_count_for(9)).to eq 13 }
-  specify { expect(BonusDrink.total_count_for(10)).to eq 14 }
-  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
-  specify { expect(BonusDrink.total_count_for(12)).to eq 17 }
-  specify { expect(BonusDrink.total_count_for(13)).to eq 19 }
-  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
-end
-=======
-  specify { expect(BonusDrink.total_count_for(5)).to eq 6 }
-  specify { expect(BonusDrink.total_count_for(6)).to eq 8 }
   specify { expect(BonusDrink.total_count_for(7)).to eq 9 }
   specify { expect(BonusDrink.total_count_for(8)).to eq 10 }
   specify { expect(BonusDrink.total_count_for(9)).to eq 12 }
@@ -37,4 +22,3 @@ end
   specify { expect(BonusDrink.total_count_for(20)).to eq 26 }
   specify { expect(BonusDrink.total_count_for(100)).to eq 133 }
 end
->>>>>>> master
